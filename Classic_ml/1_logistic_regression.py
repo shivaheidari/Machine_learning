de47853d249726c12471a0ffbb79d5bae752d731
Shiva Heydari <shivaheydari.es@gmail.com>
2025-04-03 21:22:41 -0600

    logisticregression

---
+++ Classic_ml/1_logistic_regression.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class LogisticRegresson:
+
+    def __init__(self):
+        pass
+    def _sig(x):
+        return 1/(1 + np.exp(-x))
+        pass
+    def fit(x,y):
+        pass
+    def predict(x):
+        pass
+
+
+print(1+np.exp(0))
