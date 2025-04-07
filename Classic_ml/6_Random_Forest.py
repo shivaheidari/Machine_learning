c195f6f6822a5184731acc95acf364a1ea198ce1
Shiva Heydari <shivaheydari.es@gmail.com>
2025-04-07 17:06:10 -0600

    array

--- Classic_ml/6_Random_Forest.py
+++ Classic_ml/6_Random_Forest.py
@@ -46,10 +46,33 @@ x = np.random.randint(0,40, 20).reshape(-1,1)
 y = np.random.randint(0,1,20)
 models = bagging(x,y, 3, 10)
 test_data = np.random.randint(0,40,3).reshape(-1,1)
-test_data = [[10],[20],[30]]
-for model in models:
-    for data in test_data:
-        data = np.array(data).reshape(-1,1)
-        estimate = model.predict(data)
-        print(estimate)
+test_data = [[10],[20],[30],[30]]
+# for model in models:
+#     estimates = []
+#     for data in test_data:
+#         data = np.array(data).reshape(-1,1)
+#         estimate = model.predict(data)
+#         estimates.append(estimate)
+    
+def hard_voting(x):
+    
 
+    vals, counts = np.unique(test_data, return_counts=True)
+    majority = vals[np.argmax(counts)]
+    return majority
+
+
+input = [1,2,1,1,3]
+
+counts = {}
+for item in input:
+    if item not in counts:
+        counts[item] = 1
+    else:
+        counts[item] += 1
+print(counts)
+x = [20,2,3]
+y = [2,1,4]
+zipped = zip(x, y)
+for x, y in zipped:
+    print(x,y)
---
+++ Classic_ml/arrays_for_ml.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr = np.array([1,2,4])
+z = np.zeros((3,2))
+ones = np.ones((3,2))
+print(ones)
