cda59749924877c8116294b6ca236c02d064076c
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-28 11:06:50 -0600

    dataset

--- Basic Algorithms/finetuning_approaches/standard.py
+++ Basic Algorithms/finetuning_approaches/standard.py
@@ -8,4 +8,10 @@ All parameters of the pre-trained models are trained this includs:
 
 
 """
+from huggingface_hub import login
+login()
+from datasets import load_dataset
+from transformers import AutoTokenizer
 
+dataset = load_dataset("yelp_review_full")
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-case")
