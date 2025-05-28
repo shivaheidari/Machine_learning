bba81c543acf45d2f48b86b2bfd9f0a31dfe8f37
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-28 10:52:28 -0600

    statges

--- Basic Algorithms/finetuning_approaches/standard.py
+++ Basic Algorithms/finetuning_approaches/standard.py
@@ -1,8 +1,11 @@
 """
-
-
-
-
+in the standard approach, Trainer API is used. 
+All parameters of the pre-trained models are trained this includs:  
+    Embedding layer (token, position and segment embedding)
+    attention weights in transformer layers
+    feedforward neural netwrok wieghts in each layer
+    final classification/regression head 
 
 
 """
+
